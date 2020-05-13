00046b2937cbb2299d81d7bfb33bfd490946e9e8
Muharrem Servet ANKARALI <msankarali@outlook.com>
2020-05-13 15:25:59 +0300

    builder design pattern added

---
+++ Builder/Program.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Builder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ProductDirector director = new ProductDirector();
+            var builder = new NewCustomerProductBuilder();
+            director.GenerateProduct(builder);
+            var model = builder.GetModel();
+
+            Console.WriteLine(model.Id);
+            Console.WriteLine(model.ProductName);
+            Console.WriteLine(model.CategoryName);
+            Console.WriteLine(model.DiscountApplied);
+            Console.WriteLine(model.DiscountedPrice);
+            Console.WriteLine(model.UnitPrice);
+
+            Console.ReadLine();
+        }
+    }
+
+    class ProductViewModel
+    {
+        public int Id { get; set; }
+        public string CategoryName { get; set; }
+        public string ProductName { get; set; }
+        public decimal UnitPrice { get; set; }
+        public decimal DiscountedPrice { get; set; }
+        public bool DiscountApplied { get; set; }
+    }
+
+    abstract class ProductBuilder
+    {
+        public abstract void GetProductData();
+        public abstract void ApplyDiscount();
+        public abstract ProductViewModel GetModel();
+    }
+
+    class NewCustomerProductBuilder : ProductBuilder
+    {
+        ProductViewModel model = new ProductViewModel();
+        public override void GetProductData()
+        {
+            model.Id = 1;
+            model.CategoryName = "Beverages";
+            model.ProductName = "English Tea";
+            model.UnitPrice = 20;
+        }
+
+        public override void ApplyDiscount()
+        {
+            model.DiscountedPrice = model.UnitPrice * (decimal)0.9;
+            model.DiscountApplied = true;
+        }
+
+        public override ProductViewModel GetModel()
+        {
+            return model;
+        }
+    }
+
+    class OldCustomerProductBuilder : ProductBuilder
+    {
+        ProductViewModel model = new ProductViewModel();
+        public override void GetProductData()
+        {
+            model.Id = 1;
+            model.CategoryName = "Beverages";
+            model.ProductName = "Turkish Tea";
+            model.UnitPrice = 20;
+        }
+
+        public override void ApplyDiscount()
+        {
+            model.DiscountedPrice = model.UnitPrice;
+            model.DiscountApplied = false;
+        }
+
+        public override ProductViewModel GetModel()
+        {
+            return model;
+        }
+    }
+
+    class ProductDirector
+    {
+        public void GenerateProduct(ProductBuilder productBuilder)
+        {
+            productBuilder.GetProductData();
+            productBuilder.ApplyDiscount();
+        }
+    }
+}
--- Builder/Properties/AssemblyInfo.cs
+++ Builder/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("DesignPatterns")]
+[assembly: AssemblyTitle("Builder")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("DesignPatterns")]
+[assembly: AssemblyProduct("Builder")]
 [assembly: AssemblyCopyright("Copyright ©  2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("d0c5efe9-9cd7-411c-ad9f-b18f950567bc")]
+[assembly: Guid("e9b5b3f1-5d97-483b-bcf5-df0cb705feb5")]
 
 // Version information for an assembly consists of the following four values:
 //
