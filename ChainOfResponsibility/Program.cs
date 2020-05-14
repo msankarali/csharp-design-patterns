ddafc1bdfae217d6b2274c42f832089efba7c11e
Muharrem Servet ANKARALI <msankarali@outlook.com>
2020-05-15 00:49:17 +0300

    Chain of Responsibility dp added

---
+++ ChainOfResponsibility/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChainOfResponsibility
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Manager manager = new Manager();
+            VicePresident vicePresident = new VicePresident();
+            President president = new President();
+
+            manager.SetSuccessor(vicePresident);
+            vicePresident.SetSuccessor(president);
+
+            Expense expense = new Expense{Detail = "Training", Amount = 98};
+            Expense expense2 = new Expense{Detail = "Training", Amount = 150};
+            Expense expense3 = new Expense{Detail = "Training", Amount = 1200};
+            manager.HandleExpense(expense);
+            manager.HandleExpense(expense2);
+            manager.HandleExpense(expense3);
+
+            Console.ReadLine();
+        }
+    }
+
+    class Expense
+    {
+        public string Detail { get; set; }
+        public decimal Amount { get; set; }
+    }
+
+    abstract class ExpenseHandlerBase
+    {
+        protected ExpenseHandlerBase Successor;
+        public abstract void HandleExpense(Expense expense);
+
+        public void SetSuccessor(ExpenseHandlerBase successor)
+        {
+            Successor = successor;
+        }
+    }
+
+    class Manager : ExpenseHandlerBase
+    {
+        public override void HandleExpense(Expense expense)
+        {
+            if (expense.Amount <= 100)
+            {
+                Console.WriteLine("Manager handled the expense");
+            }
+            else if (Successor != null)
+            {
+                Successor.HandleExpense(expense);
+            }
+        }
+    }
+
+    class VicePresident : ExpenseHandlerBase
+    {
+        public override void HandleExpense(Expense expense)
+        {
+            if (expense.Amount > 100 && expense.Amount <= 1000)
+            {
+                Console.WriteLine("Vice President handled the expense");
+            }
+            else if (Successor != null)
+            {
+                Successor.HandleExpense(expense);
+            }
+        }
+    }
+
+    class President : ExpenseHandlerBase
+    {
+        public override void HandleExpense(Expense expense)
+        {
+            if (expense.Amount > 1000)
+            {
+                Console.WriteLine("President handled the expense");
+            }
+        }
+    }
+}
