22769e4d7f0f92562f7651726e1672b8859632bd
Muharrem Servet ANKARALI <msankarali@outlook.com>
2020-05-15 01:07:38 +0300

    Memento dp added

---
+++ Momento/Program.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Momento
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Book book = new Book
+            {
+                Isbn = "432432432",
+                Title = "Sefiller",
+                Author = "Victor Hugo"
+            };
+
+            book.ShowBook();
+            CareTaker history = new CareTaker();
+            history.Memento = book.CreateUndo();
+
+            book.Isbn = "12312321";
+            book.Title = "VICTOR HUGO";
+            book.ShowBook();
+
+            book.RestoreFromUndo(history.Memento);
+            book.ShowBook();
+
+            Console.ReadLine();
+        }
+    }
+
+    class Book
+    {
+        private string _title;
+        private string _author;
+        private string _isbn;
+        private DateTime _lastEdited;
+
+        public string Title
+        {
+            get => _title;
+            set
+            {
+                _title = value;
+                SetLastEdited();
+            }
+        }
+
+        public string Author
+        {
+            get => _author;
+            set
+            {
+                _author = value;
+                SetLastEdited();
+            }
+        }
+
+        public string Isbn
+        {
+            get => _isbn;
+            set
+            {
+                _isbn = value;
+                SetLastEdited();
+            }
+        }
+
+        private void SetLastEdited()
+        {
+            _lastEdited = DateTime.UtcNow;
+        }
+
+        public Memento CreateUndo()
+        {
+            return new Memento(_title, _author, _isbn, _lastEdited);
+        }
+
+        public void RestoreFromUndo(Memento memento)
+        {
+            _title = memento.Title;
+            _author = memento.Author;
+            _isbn = memento.Isbn;
+            _lastEdited = memento.LastEdited;
+        }
+
+        public void ShowBook()
+        {
+            Console.WriteLine("{0}, {1}, {2} edited: {3}", Isbn, Title, Author, _lastEdited);
+        }
+    }
+
+    class Memento
+    {
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public string Isbn { get; set; }
+        public DateTime LastEdited { get; set; }
+
+        public Memento(string title, string author, string isbn, DateTime lastEdited)
+        {
+            Title = title;
+            Author = author;
+            Isbn = isbn;
+            LastEdited = lastEdited;
+        }
+    }
+
+    class CareTaker
+    {
+        public Memento Memento { get; set; }
+    }
+}
