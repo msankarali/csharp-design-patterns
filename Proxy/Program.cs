48cbea11d3a327287a2518518ba30ed33976cee9
Muharrem Servet ANKARALI <msankarali@outlook.com>
2020-05-14 22:43:36 +0300

    proxy dp added

---
+++ Proxy/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Proxy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CreditBase creditManager = new CreditManagerProxy();
+            Console.WriteLine(creditManager.Calculate());
+            Console.WriteLine(creditManager.Calculate());
+
+            Console.ReadLine();
+        }
+    }
+
+    abstract class CreditBase
+    {
+        public abstract int Calculate();
+    }
+
+    class CreditManager : CreditBase
+    {
+        public override int Calculate()
+        {
+            int result = 1;
+            for (int i = 1; i < 5; i++)
+            {
+                result *= i;
+                Thread.Sleep(1000);
+            }
+
+            return result;
+        }
+    }
+
+    class CreditManagerProxy : CreditBase
+    {
+        private CreditManager _creditManager;
+        private int _cachedValue;
+        public override int Calculate()
+        {
+            if (_creditManager == null)
+            {
+                _creditManager = new CreditManager();
+                _cachedValue = _creditManager.Calculate();
+            }
+
+            return _cachedValue;
+        }
+    }
+}
