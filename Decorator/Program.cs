9e235dcb7c998257f5869e4e27f88bcf91b9b3ef
Muharrem Servet ANKARALI <msankarali@outlook.com>
2020-05-14 23:04:21 +0300

    Decorator dp added

---
+++ Decorator/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Decorator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var personalCar = new PersonalCar
+            {
+                Make = "BMW",
+                Model = "730",
+                HirePrice = 120
+            };
+
+            SpecialOffer specialOffer = new SpecialOffer(personalCar) { DiscountPercentage = 10 };
+            Console.WriteLine("Concrete: {0}", personalCar.HirePrice);
+            Console.WriteLine("Special offer: {0}", specialOffer.HirePrice);
+
+            Console.ReadLine();
+        }
+    }
+
+    abstract class CarBase
+    {
+        public abstract string Make { get; set; }
+        public abstract string Model { get; set; }
+        public abstract decimal HirePrice { get; set; }
+    }
+
+    class PersonalCar : CarBase
+    {
+        public override string Make { get; set; }
+        public override string Model { get; set; }
+        public override decimal HirePrice { get; set; }
+    }
+
+    class CommercialCar : CarBase
+    {
+        public override string Make { get; set; }
+        public override string Model { get; set; }
+        public override decimal HirePrice { get; set; }
+    }
+
+    abstract class CarDecoratorBase : CarBase
+    {
+        private CarBase _carBase;
+
+        protected CarDecoratorBase(CarBase carBase)
+        {
+            _carBase = carBase;
+        }
+    }
+
+    class SpecialOffer : CarDecoratorBase
+    {
+        public int DiscountPercentage { get; set; }
+        private readonly CarBase _carBase;
+
+        public SpecialOffer(CarBase carBase) : base(carBase)
+        {
+            _carBase = carBase;
+        }
+
+        public override string Make { get; set; }
+        public override string Model { get; set; }
+
+        public override decimal HirePrice
+        {
+            get
+            {
+                return _carBase.HirePrice - _carBase.HirePrice * DiscountPercentage / 100;
+            }
+            set
+            {
+
+            }
+        }
+    }
+}
