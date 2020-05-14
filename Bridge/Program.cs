c222eff2b4b525f12ecfcbb0d26ce0ebd41a3df7
Muharrem Servet ANKARALI <msankarali@outlook.com>
2020-05-14 23:39:26 +0300

    Bridge dp added

---
+++ Bridge/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bridge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CustomerManager customerManager = new CustomerManager();
+            customerManager.MessageSenderBase = new EmailSender();
+            //customerManager.MessageSenderBase = new SmsSender();
+            customerManager.UpdateCustomer();
+            Console.ReadLine();
+        }
+    }
+
+    abstract class MessageSenderBase
+    {
+        public void Save()
+        {
+            Console.WriteLine("Message saved");
+        }
+
+        public abstract void Send(Body body);
+    }
+
+    class Body
+    {
+        public string Title { get; set; }
+        public string Message { get; set; }
+    }
+
+    class SmsSender : MessageSenderBase
+    {
+        public override void Send(Body body)
+        {
+            Console.WriteLine("{0} was sent via SmsSender", body.Title);
+        }
+    }
+
+    class EmailSender : MessageSenderBase
+    {
+        public override void Send(Body body)
+        {
+            Console.WriteLine("{0} was sent via EmailSender", body.Title);
+        }
+    }
+
+    class CustomerManager
+    {
+        public MessageSenderBase MessageSenderBase { get; set; }
+        public void UpdateCustomer()
+        {
+            MessageSenderBase.Send(new Body { Title = "About your update" });
+            Console.WriteLine("Customer updated");
+        }
+    }
+}
