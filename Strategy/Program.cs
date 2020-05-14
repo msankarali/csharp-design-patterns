f91ea889c58cdd1b1bdf3d01acdc750e51f09496
Muharrem Servet ANKARALI <msankarali@outlook.com>
2020-05-14 23:47:51 +0300

    Strategy dp added

---
+++ Strategy/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Strategy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CustomerManager customerManager = new CustomerManager();
+            customerManager.CreditCalculatorBase = new Before2010CreditCalculator();
+            customerManager.SaveCredit();
+
+            customerManager.CreditCalculatorBase = new After2010CreditCalculator();
+            customerManager.SaveCredit();
+            Console.ReadLine();
+        }
+    }
+
+    abstract class CreditCalculatorBase
+    {
+        public abstract void Calculate();
+    }
+
+    class Before2010CreditCalculator : CreditCalculatorBase
+    {
+        public override void Calculate()
+        {
+            Console.WriteLine("Credit Calculated using before 2010");
+        }
+    }
+
+    class After2010CreditCalculator : CreditCalculatorBase
+    {
+        public override void Calculate()
+        {
+            Console.WriteLine("Credit Calculated using after 2010");
+        }
+    }
+
+    class CustomerManager
+    {
+        public CreditCalculatorBase CreditCalculatorBase { get; set; }
+        public void SaveCredit()
+        {
+            Console.WriteLine("Customer Manager Business");
+            CreditCalculatorBase.Calculate();
+        }
+    }
+}
