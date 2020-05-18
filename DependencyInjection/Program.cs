8a0e4af9fdfac8c704e950cf0a4cd32232022383
Muharrem Servet ANKARALI <msankarali@outlook.com>
2020-05-18 03:02:37 +0300

    Dependency Injection design pattern added

---
+++ DependencyInjection/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Ninject;
+
+namespace DependencyInjection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IKernel kernel = new StandardKernel();
+            kernel.Bind<IProductDal>().To<EfProductDal>().InSingletonScope();
+
+            ProductManager productManager = new ProductManager(kernel.Get<IProductDal>());
+            productManager.Save();
+
+            Console.ReadLine();
+        }
+    }
+
+    interface IProductDal
+    {
+        void Save();
+    }
+
+    class EfProductDal : IProductDal
+    {
+        public void Save()
+        {
+            Console.WriteLine("Saved with Ef");
+        }
+    }
+
+    class NhProductDal : IProductDal
+    {
+        public void Save()
+        {
+            Console.WriteLine("Saved with Nh");
+        }
+    }
+
+    class ProductManager
+    {
+        private IProductDal _productDal;
+
+        public ProductManager(IProductDal productDal)
+        {
+            _productDal = productDal;
+        }
+        public void Save()
+        {
+            _productDal.Save();
+        }
+    }
+}
