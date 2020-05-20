57ccc33ed7b49394b063d078ac3e6c9bb55fbc06
Muharrem Servet ANKARALI <msankarali@outlook.com>
2020-05-20 23:47:00 +0300

    Multiton design pattern added

---
+++ Multiton/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Multiton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Camera camera1 = Camera.GetCamera("Nikon");
+            Camera camera2 = Camera.GetCamera("Canon");
+            Camera camera3 = Camera.GetCamera("Nikon");
+            Camera camera4 = Camera.GetCamera("Canon");
+            Camera camera5 = Camera.GetCamera("NIKON");
+            Camera camera6 = Camera.GetCamera("CANON");
+
+            Console.WriteLine(camera1.Id);
+            Console.WriteLine(camera2.Id);
+            Console.WriteLine(camera3.Id);
+            Console.WriteLine(camera4.Id);
+            Console.WriteLine(camera5.Id);
+            Console.WriteLine(camera6.Id);
+
+            Console.ReadLine();
+
+        }
+    }
+
+    class Camera
+    {
+        static Dictionary<string, Camera> _cameras = new Dictionary<string, Camera>();
+        static object _lock = new object();
+        public Guid Id { get; set; }
+
+        private Camera()
+        {
+            Id = Guid.NewGuid();
+        }
+
+        public static Camera GetCamera(string brand)
+        {
+            lock (_lock)
+            {
+                if (!_cameras.ContainsKey(brand))
+                {
+                    _cameras.Add(brand, new Camera());
+                }
+            }
+
+            return _cameras[brand];
+        }
+    }
+}
